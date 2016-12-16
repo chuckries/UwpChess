3ad132b6c5ba00a95108cdd75f6dbd9bf84b5ae2
Chuck Ries <chuck.ries@gmail.com>
2016-12-16 11:42:50 -0800

    Add project files.

---
+++ UwpChess/BindingHelper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace UwpChess
+{
+    public class BindingHelper
+    {
+        public static readonly DependencyProperty GridColumnBindingPathProperty =
+            DependencyProperty.RegisterAttached(
+                "GridColumnBindingPath", typeof(string), typeof(BindingHelper),
+                new PropertyMetadata(null, GridBindingPathPropertyChanged));
+
+        public static readonly DependencyProperty GridRowBindingPathProperty =
+            DependencyProperty.RegisterAttached(
+                "GridRowBindingPath", typeof(string), typeof(BindingHelper),
+                new PropertyMetadata(null, GridBindingPathPropertyChanged));
+
+        public static string GetGridColumnBindingPath(DependencyObject obj)
+        {
+            return (string)obj.GetValue(GridColumnBindingPathProperty);
+        }
+
+        public static void SetGridColumnBindingPath(DependencyObject obj, string value)
+        {
+            obj.SetValue(GridColumnBindingPathProperty, value);
+        }
+
+        public static string GetGridRowBindingPath(DependencyObject obj)
+        {
+            return (string)obj.GetValue(GridRowBindingPathProperty);
+        }
+
+        public static void SetGridRowBindingPath(DependencyObject obj, string value)
+        {
+            obj.SetValue(GridRowBindingPathProperty, value);
+        }
+
+        private static void GridBindingPathPropertyChanged(
+            DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var propertyPath = e.NewValue as string;
+
+            if (propertyPath != null)
+            {
+                var gridProperty =
+                    e.Property == GridColumnBindingPathProperty
+                    ? Grid.ColumnProperty
+                    : Grid.RowProperty;
+
+                BindingOperations.SetBinding(
+                    obj,
+                    gridProperty,
+                    new Binding { Path = new PropertyPath(propertyPath) });
+            }
+        }
+    }
+}
---
+++ UwpChess/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UwpChess
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public BoardViewModel ViewModel { get; private set; }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ViewModel = new BoardViewModel();
+        }
+    }
+
+
+
+    public class BoardViewModel
+    {
+        public IReadOnlyCollection<TileViewModel> Tiles { get; private set; }
+
+        public BoardViewModel()
+        {
+            List<TileViewModel> tiles = new List<TileViewModel>(64);
+            for (int i = 0; i < 64; i++)
+            {
+                tiles.Add(new TileViewModel(this, i));
+            }
+            Tiles = tiles.AsReadOnly();
+        }
+    }
+
+    public enum TileColor
+    {
+        Light,
+        Dark
+    }
+
+    public class TileCoordinate
+    {
+        public int Id { get; private set; }
+        public int Row { get; private set; }
+        public int Col { get; private set; }
+        public TileColor Color { get; private set;}
+        public string Notation { get; private set; }
+
+        private static string[] Ranks = { "8", "7", "6", "5", "4", "3", "2", "1" };
+        private static string[] Files = { "A", "B", "C", "D", "E", "F", "G", "H" };
+
+        public TileCoordinate(int id)
+        {
+            Id = id;
+            Row = Id / 8;
+            Col = Id % 8;
+            Color = (Row + Col) % 2 == 0 ? TileColor.Light : TileColor.Dark;
+            Notation = Files[Col] + Ranks[Row];
+        }
+    }
+
+    public class TileViewModel : BindableBase
+    {
+        private ChessPiece _piece;
+
+        public BoardViewModel Board { get; private set; }
+        public TileCoordinate Coordinate { get; private set; }
+        public int Id { get { return Coordinate.Id; } }
+
+        public ChessPiece Piece
+        {
+            get { return _piece; }
+            set { SetPropertyValue(ref _piece, value); }
+        }
+
+        public TileViewModel(BoardViewModel board, int id)
+        {
+            Board = board;
+            Coordinate = new TileCoordinate(id);
+        }
+    }
+
+    public class TileStyleSelector : StyleSelector
+    {
+        public Style Light { get; set; }
+        public Style Dark { get; set; }
+        protected override Style SelectStyleCore(object item, DependencyObject container)
+        {
+            TileViewModel tile = item as TileViewModel;
+
+            if (tile.Coordinate.Color == TileColor.Dark) return Dark;
+            if (tile.Coordinate.Color == TileColor.Light) return Light;
+
+            return base.SelectStyleCore(item, container);
+        }
+    }
+
+    public class VisiblilityValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool visible = (bool)value;
+            if (visible)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public enum PieceType
+    {
+        Pawn,
+        Knight,
+        Bishop,
+        Rook,
+        Queen,
+        King
+    }
+
+    public enum PieceColor
+    {
+        White,
+        Black
+    }
+
+    public class ChessPiece : BindableBase
+    {
+        private PieceType _type;
+        private PieceColor _color;
+
+        public PieceType Type
+        {
+            get { return _type; }
+            set { SetPropertyValue(ref _type, value); }
+        }
+
+        public PieceColor Color
+        {
+            get { return _color; }
+            set { SetPropertyValue(ref _color, value); }
+        }
+    }
+
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual bool SetPropertyValue<T>(ref T originalValue, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(originalValue, newValue))
+            {
+                originalValue = newValue;
+                OnPropertyChanged(propertyName);
+                return true;
+            }
+            return false;
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
